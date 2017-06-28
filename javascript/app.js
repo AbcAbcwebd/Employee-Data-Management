54a7a7eef251f62ecb82541b42da9f59b95846ab
Chris Rackley <misterrackley@gmail.com>
2017-06-28 19:48:17 -0400

    routing fix

--- javascript/app.js
+++ javascript/app.js
@@ -1,20 +1,24 @@
 // This function captures what the user typed on the form and saves it to the database.
 // It should be a seperate function outside of a click event so that it can be called both for the click event and for a keydown event on 'ENTER'
+var database = firebase.database();
+
 function captureInput(){
 	// These should be set equal to jQuery selectors that capture necesary data.
-<<<<<<< HEAD
 	var employeeName = $("#employeeAdd").val().trim();
 	var role = $("#role").val().trim();
 	var startDate = $("#startDate").val().trim();
 	var monthlyRate = $("#monthlyRate").val().trim();
-=======
-	var employeeName = $('#employeeAdd').val();
-	var role = $('#role').val();
-	var startDate = $('#startDate').val();
-	var monthlyRate = $('#monthlyRate').val();
->>>>>>> 9423a0530057cbd8dd6c7c4def0db7e1c712e433
 
-	// Info needs to be saved to Firebase
+	database.push({
+		employeeName: employeeName,
+		role: role,
+		startDate: startDate,
+		monthlyRate: startDate,
+		dateAdded: firebase.database.ServerValue.TIMESTAMP
+	});
+
+	
+	
 };
 
 $('document').ready(function(){
